10175c8ae93ad443ab795b234e19436d8eb0ccda
Michael Leibbrandt <mike.leibbrandt@gmail.com>
2024-09-01 13:34:41 +0200

    feat: sign-up form implementation

--- app/components/forms/sign-up/index.tsx
+++ app/components/forms/sign-up/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SubmitHandler, useForm } from "react-hook-form";
+import Heading from "../../heading";
 
 export type FormValues = {
   firstName: string;
@@ -23,7 +24,105 @@ const SignUpForm = () => {
     console.log("Form Data:", data);
   };
 
-  return <></>;
+  return (
+    <form
+      className="rounded bg-neutral px-8 pt-6 pb-8 mb-4 w-[45vw]"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="mb-4 ">
+        <label className="block text-sm font-bold mb-2" htmlFor="username">
+          Username
+        </label>
+        <input
+          id="username"
+          type="text"
+          placeholder="Username"
+          className={`input input-bordered w-full ${errors.username ? "input-error" : ""}`}
+          {...register("username", { required: "Username is required" })}
+        />
+        {errors.username && (
+          <p className="text-red-500 text-xs italic">
+            {errors.username.message}
+          </p>
+        )}
+      </div>
+
+      <div className="mb-4 ">
+        <label className="block text-sm font-bold mb-2" htmlFor="username">
+          First Name
+        </label>
+        <input
+          id="firstName"
+          type="text"
+          placeholder="First Name"
+          className={`input input-bordered w-full ${errors.username ? "input-error" : ""}`}
+          {...register("firstName", { required: "First Name is required" })}
+        />
+        {errors.firstName && (
+          <p className="text-red-500 text-xs italic">
+            {errors.firstName.message}
+          </p>
+        )}
+      </div>
+
+      <div className="mb-4 ">
+        <label className="block text-sm font-bold mb-2" htmlFor="username">
+          Last Name
+        </label>
+        <input
+          id="lastName"
+          type="text"
+          placeholder="Last Name"
+          className={`input input-bordered w-full ${errors.username ? "input-error" : ""}`}
+          {...register("lastName", { required: "Last Name is required" })}
+        />
+        {errors.lastName && (
+          <p className="text-red-500 text-xs italic">
+            {errors.lastName.message}
+          </p>
+        )}
+      </div>
+
+      <div className="mb-4 ">
+        <label className="block text-sm font-bold mb-2" htmlFor="username">
+          Email Address
+        </label>
+        <input
+          id="email"
+          type="text"
+          placeholder="Email Addres"
+          className={`input input-bordered w-full ${errors.username ? "input-error" : ""}`}
+          {...register("email", { required: "Email Address is required" })}
+        />
+        {errors.email && (
+          <p className="text-red-500 text-xs italic">{errors.email.message}</p>
+        )}
+      </div>
+
+      <div className="mb-6">
+        <label className="block text-sm font-bold mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          className={`input input-bordered w-full ${errors.password ? "input-error" : ""}`}
+          {...register("password", { required: "Password is required" })}
+        />
+        {errors.password && (
+          <p className="text-red-500 text-xs italic">
+            {errors.password.message}
+          </p>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <button type="submit" className="btn btn-primary w-full mb-4">
+          Sign Up
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default SignUpForm;
--- app/users/sign-up/page.tsx
+++ app/users/sign-up/page.tsx
@@ -1,7 +1,13 @@
+import SignUpForm from "@/app/components/forms/sign-up";
 import Heading from "@/app/components/heading";
 
 const SignUpPage = () => {
-  return <Heading text="Sign up" />;
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <Heading text="Sign up" />
+      <SignUpForm />
+    </div>
+  );
 };
 
 export default SignUpPage;
