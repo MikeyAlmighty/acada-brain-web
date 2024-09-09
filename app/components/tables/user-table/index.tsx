e895515ef420c3d901350060bb7a79297fc949f9
Michael Leibbrandt <mike.leibbrandt@gmail.com>
2024-09-09 21:45:28 +0200

    feat: added Learners Table

--- app/components/tables/user-table/index.tsx
+++ app/components/tables/user-table/index.tsx
@@ -1,5 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 type LearnerTableProps = {
   learners: {
+    id: string;
     firstName: string;
     lastName: string;
     username: string;
@@ -7,21 +12,43 @@ type LearnerTableProps = {
 };
 
 const LearnerTable = ({ learners }: LearnerTableProps) => {
+  const [selectedLearnerIds, setSelectedLearnerIds] = useState<strin[]>([]);
+  console.log(selectedLearnerIds);
+
+  const handleLearnerSelect = (learnerId: string) => {
+    if (selectedLearnerIds.includes(learnerId)) {
+      setSelectedLearnerIds([
+        ...selectedLearnerIds.filter((id) => id !== learnerId),
+      ]);
+    } else {
+      setSelectedLearnerIds([...selectedLearnerIds, learnerId]);
+    }
+  };
+
   return (
     <div className="overflow-x-auto">
       <table className="table">
         {/* head */}
         <thead>
           <tr>
-            {/* <th>Avatar</th> */}
+            <th></th>
             <th>Username</th>
             <th>First Name</th>
             <th>Last Name</th>
           </tr>
         </thead>
         <tbody>
-          {learners.map(({ firstName, lastName, username }) => (
+          {learners.map(({ id, firstName, lastName, username }) => (
             <tr className="hover">
+              <th>
+                <label>
+                  <input
+                    type="checkbox"
+                    onClick={() => handleLearnerSelect(id)}
+                    className="checkbox checkbox-primary"
+                  />
+                </label>
+              </th>
               <th>{firstName}</th>
               <th>{lastName}</th>
               <th>{username}</th>
